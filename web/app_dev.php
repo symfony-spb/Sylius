6ba421e32ec7d8e4944db3fc98fe80cc76352d9c
Denis Manilo <d.manilo@iconto.ru>
2014-08-25 19:47:33 +0400

    app_dev opened

--- web/app_dev.php
+++ web/app_dev.php
@@ -16,7 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
  * Sylius front controller.
  * Dev environment.
  */
-
+/*
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '172.33.33.1',
@@ -26,7 +26,7 @@ if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-
+*/
 require_once __DIR__.'/../app/bootstrap.php.cache';
 
 Debug::enable();
